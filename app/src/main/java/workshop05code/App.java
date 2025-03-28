fc4a9bbf2c84ddaef8dfd772dd5419ab20c4eae6
Amy Nguyen <thianhminh.nguyen@students.mq.edu.au>
2025-03-28 16:20:58 +1100

    Implement logging policy

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -17,17 +17,18 @@ import java.util.logging.Logger;
  */
 public class App {
     // Start code for logging exercise
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
     static {
         // must set before the Logger
         // loads logging.properties from the classpath
         try {// resources\logging.properties
             LogManager.getLogManager().readConfiguration(new FileInputStream("resources/logging.properties"));
         } catch (SecurityException | IOException e1) {
-            e1.printStackTrace();
+            logger.log(Level.SEVERE, "Failed to load logging configuration.", e1);
+            System.out.println("Failed launch Wordle.");
         }
     }
-
-    private static final Logger logger = Logger.getLogger(App.class.getName());
     // End code for logging exercise
     
     /**
@@ -37,57 +38,73 @@ public class App {
         SQLiteConnectionManager wordleDatabaseConnection = new SQLiteConnectionManager("words.db");
 
         wordleDatabaseConnection.createNewDatabase("words.db");
+
         if (wordleDatabaseConnection.checkIfConnectionDefined()) {
-            System.out.println("Wordle created and connected.");
+            logger.log(Level.INFO, "Wordle database connection established successfully.");
         } else {
-            System.out.println("Not able to connect. Sorry!");
+            logger.log(Level.SEVERE,"ERROR: Unable to establish database connection.");
+            getFailedMessage();
             return;
         }
+        
         if (wordleDatabaseConnection.createWordleTables()) {
-            System.out.println("Wordle structures in place.");
+            logger.log(Level.INFO,"Database tables created successfully.");
         } else {
-            System.out.println("Not able to launch. Sorry!");
+            logger.log(Level.SEVERE,"ERROR: Failed to create database tables.");
+            getFailedMessage();
             return;
         }
 
         // let's add some words to valid 4 letter words from the data.txt file
-
+        
         try (BufferedReader br = new BufferedReader(new FileReader("resources/data.txt"))) {
             String line;
             int i = 1;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                logger.log(Level.INFO, "Loaded word: {0}", line);
                 wordleDatabaseConnection.addValidWord(i, line);
                 i++;
             }
 
         } catch (IOException e) {
-            System.out.println("Not able to load . Sorry!");
-            System.out.println(e.getMessage());
+            logger.log(Level.SEVERE, "ERROR: Could not load words from data.txt.", e);
+            getFailedMessage();
             return;
         }
 
         // let's get them to enter a word
 
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter a 4 letter word for a guess or q to quit: ");
+            logger.log(Level.INFO, "Waiting for user's guess");
+
+            System.out.println("Welcome to Wordle");
+            System.out.print("Enter a 4 letter word for a guess or 'q' to quit: ");
+
             String guess = scanner.nextLine();
 
             while (!guess.equals("q")) {
+                logger.log(Level.INFO, "User entered: '" + guess + "'.");
                 System.out.println("You've guessed '" + guess+"'.");
 
                 if (wordleDatabaseConnection.isValidWord(guess)) { 
+                    logger.log(Level.INFO, "Valid guess!");
                     System.out.println("Success! It is in the the list.\n");
                 }else{
+                    logger.log(Level.WARNING, "Invalid guess!");
                     System.out.println("Sorry. This word is NOT in the the list.\n");
                 }
 
-                System.out.print("Enter a 4 letter word for a guess or q to quit: " );
+                logger.log(Level.INFO, "Waiting for user's guess");
+                System.out.print("Enter a 4 letter word for a guess or 'q' to quit: " );
                 guess = scanner.nextLine();
             }
         } catch (NoSuchElementException | IllegalStateException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Unexpected input error.", e);
+            getFailedMessage();
         }
+    }
 
+    private static void getFailedMessage() {
+        System.out.println("Unexpected error occurred. Exiting Wordle.");
     }
 }
--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -65,12 +65,11 @@ public class SQLiteConnectionManager {
     try (Connection conn = DriverManager.getConnection(databaseURL)) {
       if (conn != null) {
         DatabaseMetaData meta = conn.getMetaData();
-        System.out.println("The driver name is " + meta.getDriverName());
-        System.out.println("A new database has been created.");
-
+        logger.log(Level.INFO, "The driver name is " + meta.getDriverName());
+        logger.log(Level.INFO, "A new database has been created.");
       }
     } catch (SQLException e) {
-      System.out.println(e.getMessage());
+      logger.log(Level.SEVERE, "ERROR: Failed to create new database.", e);
     }
   }
 
@@ -89,7 +88,7 @@ public class SQLiteConnectionManager {
           return true;
         }
       } catch (SQLException e) {
-        System.out.println(e.getMessage());
+        logger.log(Level.SEVERE, "ERROR: Failed to connect to database.", e);
         return false;
       }
     }
@@ -114,7 +113,7 @@ public class SQLiteConnectionManager {
         return true;
 
       } catch (SQLException e) {
-        System.out.println(e.getMessage());
+        logger.log(Level.SEVERE, "ERROR: Failed to create Wordle tables." + e);
         return false;
       }
     }
@@ -136,7 +135,7 @@ public class SQLiteConnectionManager {
       pstmt.setString(2, word);
       pstmt.executeUpdate();
     } catch (SQLException e) {
-      System.out.println(e.getMessage());
+      logger.log(Level.SEVERE, "ERROR: Failed to add valid word to database." + e);
     }
 
   }
@@ -162,7 +161,7 @@ public class SQLiteConnectionManager {
       return false;
 
     } catch (SQLException e) {
-      System.out.println(e.getMessage());
+      logger.log(Level.SEVERE, "ERROR: Failed to check if word is valid." + e);
       return false;
     }
 
